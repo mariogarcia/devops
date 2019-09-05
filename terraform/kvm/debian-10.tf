896cca5dbfb927e430f0cce88d11e2c1a0fdf9d6
Mario Garcia <mario.ggar@gmail.com>
2019-09-06 01:37:50 +0200

    Fix terraform directory structure

--- terraform/kvm/debian-10.tf
+++ terraform/kvm/debian-10.tf
@@ -1,21 +1,21 @@
 provider "libvirt" {
-  uri   = "qemu+ssh://maintainer@jason.org/system"
+  uri   = "qemu+ssh://maintainer@future/system"
 }
 
-resource "libvirt_pool" "debian9" {
-  name = "debian9"
+resource "libvirt_pool" "debian10" {
+  name = "debian10"
   type = "dir"
-  path = "/tmp/terraform-provider-libvirt-pool-debian9"
+  path = "/tmp/terraform-provider-libvirt-pool-debian10"
 }
 
 resource "libvirt_volume" "os_image" {
   name   = "os_image"
-  pool   = "${libvirt_pool.debian9.name}"
-  source = "/home/mario/Development/repositories/hashicorp-tools/packer/output_debian9/debian9"
+  pool   = "${libvirt_pool.debian10.name}"
+  source = "/home/mario/Development/repositories/hashicorp-tools/packer/output_debian10/debian10"
 }
 
-resource "libvirt_domain" "debian9" {
-  name = "debian9"
+resource "libvirt_domain" "debian10" {
+  name = "debian10"
 
   disk {
     volume_id = "${libvirt_volume.os_image.id}"
